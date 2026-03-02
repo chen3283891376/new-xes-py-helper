f5e17eb0fda44fb1edeaa9a96f0323ecb2de8660
chen3283891376 <3283891376@qq.com>
2026-03-02 15:28:37 +0800

    fix: 顶层await在编译时出问题了

--- index.ts
+++ index.ts
@@ -3,22 +3,24 @@ import { PythonProcessManager } from './src/python';
 import { RouteHandler } from './src/route_handler';
 import { createWsServer } from './src/websocket';
 
-let port: number = 55820;
+(async () => {
+	let port: number = 55820;
 
-if (await check()) {
-	port = get_port();
-	console.log('端口检查通过', port, port + 1);
-} else {
-	console.log('端口检查失败');
-	process.exit(1);
-}
+	if (await check()) {
+		port = get_port();
+		console.log('端口检查通过', port, port + 1);
+	} else {
+		console.log('端口检查失败');
+		process.exit(1);
+	}
 
-const httpServer = Bun.serve({
-	port: port,
-	hostname: '127.0.0.1',
-	routes: RouteHandler.getRoutes(),
-});
+	const httpServer = Bun.serve({
+		port: port,
+		hostname: '127.0.0.1',
+		routes: RouteHandler.getRoutes(),
+	});
 
-const pythonProcess = new PythonProcessManager();
+	const pythonProcess = new PythonProcessManager();
 
-const { server: wsServer } = createWsServer(port + 1, pythonProcess);
+	const { server: wsServer } = createWsServer(port + 1, pythonProcess);
+})();
